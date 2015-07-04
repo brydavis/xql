234ad4da0dfbfb920557cffdf18f0bebb8173867
brydavis <bryan@stax.io>
2015-07-03 21:01:58 -0700

    auto commit

--- xml/main.go
+++ xml/main.go
@@ -16,35 +16,30 @@ import (
 - Integrate into "xql" package
 */
 
-// var final = ""
-
 type Node struct {
 	XMLName xml.Name
-	Gender  xml.Attr `xml:"gender,attr"`
-	Content []byte   `xml:",innerxml"`
-	Nodes   []Node   `xml:",any"`
+	Content []byte `xml:",innerxml"`
+	Nodes   []Node `xml:",any"`
+	// HardCopy bool
+	// FileName string
 }
 
 func main() {
-	n := Nodify("xml/complex.xml")
-	n.Write("json/complex.json", 0700)
-	// n.Pretty(0)
-
-	// fmt.Println(final)
+	n := Nodify("../data/hmis.xml")
+	n.Write("../data/hmis.json", 0700)
+	n.Pretty(0)
 
 }
 
-func Nodify(filename string) Node {
+func Nodify(filename string) (n Node) {
 	b, _ := ioutil.ReadFile(filename)
+	reader := strings.NewReader(Elementize(xml.NewDecoder(strings.NewReader(string(b)))))
 
-	// buf := bytes.NewBuffer(data)
-	reader := strings.NewReader(string(b))
+	xml.NewDecoder(reader).Decode(&n)
+	return
+}
 
-	// dec := xml.NewDecoder(buf)
-	dec := xml.NewDecoder(reader)
-
-	var data string
-	// var tabs int
+func Elementize(dec *xml.Decoder) (data string) {
 	for {
 		token, err := dec.Token()
 		if err != nil {
@@ -53,71 +48,31 @@ func Nodify(filename string) Node {
 
 		switch t := token.(type) {
 		case xml.StartElement:
-			// var indent string
-			// for i := 0; i < tabs; i++ {
-			// 	indent += "\t"
-			// }
-			// tabs += 1
-
 			data += fmt.Sprintf("<%s>", t.Name.Local)
-
-			// var attrs string
 			for _, v := range t.Attr {
-				// attrs += fmt.Sprintf("%s\t<%s>%s</%s>\n",  v.Name.Local, v.Value, v.Name.Local)
 				data += fmt.Sprintf("<%s>%s</%s>", v.Name.Local, v.Value, v.Name.Local)
-
 			}
 
-			// fmt.Println(attrs)
-
 		case xml.EndElement:
-			// tabs -= 1
-			// var indent string
-			// for i := 0; i < tabs; i++ {
-			// 	indent += "\t"
-			// }
-
 			data += fmt.Sprintf("</%s>", t.Name.Local)
 
 		case xml.CharData:
 			val := strings.Replace(strings.TrimSpace(string(xml.CharData(t))), "\n", "", -1)
-			fmt.Println(val)
 			if val != "" {
-				// var indent string
-				// for i := 0; i < tabs; i++ {
-				// 	indent += "\t"
-				// }
-
-				// data += fmt.Sprintf("%s<value>%s</value>\n", indent, val)
 				data += fmt.Sprintf("<value>%s</value>", val)
-
 			}
 
 		default:
 			// fmt.Println(t)
 
 		}
-
 	}
-
-	// data, _ := ioutil.ReadFile(filename)
-
-	// buf := bytes.NewBuffer(data)
-	reader = strings.NewReader(data)
-
-	// dec := xml.NewDecoder(buf)
-	dec = xml.NewDecoder(reader)
-
-	var n Node
-	dec.Decode(&n)
-	return n
-
+	return
 }
 
 func (n Node) Walk() interface{} {
 	if len(n.Nodes) < 1 {
 		return string(n.Content)
-		// final += fmt.Sprintf("<%s>%s</%s>", n.XMLName.Local, string(n.Content), n.XMLName.Local)
 
 	} else {
 		x := make(map[string][]interface{})
@@ -128,10 +83,7 @@ func (n Node) Walk() interface{} {
 
 		y := make(map[string]interface{})
 		for k, v := range x {
-
 			if len(x[k]) == 1 {
-				// final += fmt.Sprintf("<%s>%s</%s>", k, v, k)
-
 				switch v[0].(type) {
 				case float32, float64:
 					f, _ := strconv.ParseFloat(v[0].(string), 64)
@@ -169,15 +121,15 @@ func (n Node) Pretty(indent int) {
 
 	for k, v := range n.Nodes {
 		if len(v.Nodes) < 1 {
-			fmt.Printf("%s(%d) %s == content (%s)\tAttr => %v\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)), string(v.Content), v.Gender)
+			fmt.Printf("%s(%d) %s == content (%s)\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)), string(v.Content))
 		} else {
-			fmt.Printf("%s(%d) %s =>\tAttr => %v\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)), v.Gender)
+			fmt.Printf("%s(%d) %s =>\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)))
 			v.Pretty(indent + 1)
 		}
 	}
 }
 
-func (n Node) Write(name string, perm os.FileMode) {
+func (n *Node) Write(name string, perm os.FileMode) {
 	j := n.Import()
 	ioutil.WriteFile(name, j, perm)
 }
